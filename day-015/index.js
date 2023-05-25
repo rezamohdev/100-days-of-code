10b1782f9f9dc1b4d17f21b74b3d4faf13553743
rezamohdev <mohammadireza29@gmail.com>
2023-05-25 19:38:58 -0400

    day 015

--- day-015/index.js
+++ day-015/index.js
@@ -9,3 +9,25 @@ function solution(number) {
     }
     return sum;
 }
+const componentToHex = (c) => {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+const rgb = (r, g, b) => {
+    return `#${componentToHex(r) + componentToHex(g) + componentToHex(b)}`;
+}
+console.log(rgb(255, 255, 255)) // returns FFFFFF
+console.log(rgb(255, 255, 300)) // returns FFFFFF
+console.log(rgb(0, 0, 0)) // returns 000000
+console.log(rgb(148, 0, 211)) // returns 9400D3
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+console.log(rgbToHex(255, 255, 300)); // #0033ff

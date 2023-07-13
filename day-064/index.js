14ec5dd5ee0fb836e1f3b2f217ddcbee01237838
rezamohdev <mohammadireza29@gmail.com>
2023-07-13 10:00:26 -0400

    day 064

--- day-064/index.js
+++ day-064/index.js
@@ -1,9 +1,24 @@
-(function testAsyncError() {
+// Moved the code returning the promise with an error to an external function
+function returnPromiseError() {
+    return Promise.reject(new Error("Something went wrong..."));
+}
+
+(async function testAsyncAwaitError() {
     try {
         console.log("Function execution started");
-        return Promise.reject(new Error("Something went wrong..."));
+        await returnPromiseError(); // wait till returnPromiseError() is executed
     } catch (err) {
-        console.log(`Error ${err.name} with the message ${err.message} has occured, but we've handled it`);
+        console.error(`${err.name} with the message ${err.message} has occured, but we've handled it`);
     }
     console.log("Function execution completed successfully");
 })();
+
+// (function testAsyncError() {
+//     try {
+//         console.log("Function execution started");
+//         return Promise.reject(new Error("Something went wrong..."));
+//     } catch (err) {
+//         console.log(`Error ${err.name} with the message ${err.message} has occured, but we've handled it`);
+//     }
+//     console.log("Function execution completed successfully");
+// })();
